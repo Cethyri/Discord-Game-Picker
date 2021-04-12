30c0eace790d98acd5c3b6be3c3cee2b7f121200
Christopher Robinson <seabug7200@hotmail.com>
2021-04-11 23:54:10 -0700

    bot kinda does stuff

--- bot.py
+++ bot.py
@@ -1,24 +1,85 @@
-# bot.py
+from itertools import count
 import os
+import random
+import re
+from typing import Match
 
 import discord
+from discord.channel import TextChannel
+from discord.flags import Intents
+from discord.guild import Guild
+from discord.member import Member
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents: Intents = discord.Intents.none()
+intents.members = True
+intents.guilds = True
+intents.messages = True
+
+client = discord.Client(intents = intents)
+
+bot.gameChannel: TextChannel = None
+bot.general: TextChannel = None
+
+bot.games: list = []
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild: Guild = discord.utils.get(client.guilds, name=GUILD)
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+    for channel in guild.channels:
+        if (channel.name == 'random-pick'):
+            bot.gameChannel = channel
+        if (channel.name == 'general'):
+            bot.general = channel
+    
+    if gameChannel == None:
+        await general.send('I couldn''t find a channel called "random-pick"')
+        
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    print(f'{message.content}')
+
+    if 'boop' in message.content:
+
+        if gameChannel != None and games.count == 0:
+            async for message in gameChannel.history():
+                if message.author != client.user:
+                    gameEntries = re.findall('(?:\r\n|^)\s+\d+\s*-\s*(.+)')
+                    for gameEntry in gameEntries:
+                        games.append(gameEntry)
+        elif gameChannel == None:
+            await message.channel.send('I couldn''t find a channel called "random-pick"')
+
+        if games.count == 0:
+            await message.channel.send('I couldn''t find any games')
+        else:
+            game = random.choice(games)
+            await message.channel.send(f'How about {game}.')
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise()
 
 client.run(TOKEN)
